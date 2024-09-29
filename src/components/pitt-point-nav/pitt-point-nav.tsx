cc57eaaff9587540c85eb0e9edfa23ce7f9587f3
Sara <saraberg702@gmail.com>
2024-09-28 20:52:16 -0400

    silly brackets and stuffs

--- src/components/pitt-point-nav/pitt-point-nav.tsx
+++ src/components/pitt-point-nav/pitt-point-nav.tsx
@@ -14,8 +14,8 @@ export const PittPointNav = ({ className }: PittPointNavProps) => {
         <div className={classNames(styles.root, className)}>
             PittPointNav
             <nav>
-                <a href="/home">Home</a> | <a href="/projects">Projects</a> |{' '}
-                <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+                <a href="/home">Home</a> | <a href="/projects">Schedule</a> |
+                <a href="/about">Gemeni</a>|<a href="/contact">Advisors</a>
             </nav>
         </div>
     );
