3f11e7df59a3a5106988815168777965f9cb05e3
Codux <codux@codux.com>
2024-09-28 20:20:44 -0400

    HEY HOES

--- src/_codux/boards/pitt-point-home.board.tsx
+++ src/_codux/boards/pitt-point-home.board.tsx
@@ -5,8 +5,8 @@ export default createBoard({
     Board: () => (
         <div>
             <nav>
-                <a href="/home">Home</a> | <a href="/projects">Projects</a> |{' '}
-                <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+                <a href="/home">Home</a> | Schedule <a href="/projects"></a> | Gemini
+                <a href="/about"></a> | Advisors <a href="/contact"></a>
             </nav>
         </div>
     ),
