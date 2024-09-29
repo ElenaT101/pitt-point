7104eadab000acaeadaf32f8d591028f59f4dc77
Codux <codux@codux.com>
2024-09-28 20:08:30 -0400

    added a main menu

--- src/_codux/boards-global-setup.ts
+++ src/_codux/boards-global-setup.ts
@@ -4,3 +4,5 @@ import '@blueprintjs/datetime2/lib/css/blueprint-datetime2.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css';
 import '@blueprintjs/table/lib/css/table.css';
+
+import 'semantic-ui-css/semantic.min.css';
---
+++ src/_codux/boards/pitt-point-home.board.tsx
@@ -0,0 +1,14 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'PittPoint Home',
+    Board: () => (
+        <div>
+            <nav>
+                <a href="/home">Home</a> | <a href="/projects">Projects</a> |{' '}
+                <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+            </nav>
+        </div>
+    ),
+    isSnippet: true,
+});
