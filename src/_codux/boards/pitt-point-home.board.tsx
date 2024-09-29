d3b014a95a7ffbd929154b47b3d91f08f0672544
Sara <saraberg702@gmail.com>
2024-09-28 20:31:14 -0400

    renamed and added spacing

--- src/_codux/boards/pitt-point-home.board.tsx
+++ src/_codux/boards/pitt-point-home.board.tsx
@@ -1,10 +1,11 @@
 import { createBoard } from '@wixc3/react-board';
+import styles from './pitt-point-home.board.module.scss';
 
 export default createBoard({
     name: 'PittPoint Home',
     Board: () => (
         <div>
-            <nav>
+            <nav className={styles.nav1}>
                 <a href="/home">Home</a> | <a href="/projects">Schedule</a> |{' '}
                 <a href="/about">Gemini</a> | <a href="/contact">Advisors</a>
             </nav>
