8e85bb749d2a3d0f2af3224870c52b55d16c62d1
Codux <codux@codux.com>
2024-09-28 20:23:35 -0400

    Changed names

--- src/_codux/boards/pitt-point-home.board.tsx
+++ src/_codux/boards/pitt-point-home.board.tsx
@@ -5,8 +5,8 @@ export default createBoard({
     Board: () => (
         <div>
             <nav>
-                <a href="/home">Home</a> | <a href="/projects">Projects</a> |{' '}
-                <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+                <a href="/home">Home</a> | <a href="/projects">Schedule</a> |{' '}
+                <a href="/about">Gemini</a> | <a href="/contact">Advisors</a>
             </nav>
         </div>
     ),
